c3584a46cb79d2fbcbb3dd9d91d8575441fe89f0
atomer <mmroad@hotmail.co.jp>
2012-05-02 19:09:13 +0900

    ニコ動改修による修正

--- todaywatch.user.js
+++ todaywatch.user.js
@@ -2,8 +2,8 @@
 // @name		Nico today Watch
 // @namespace   http://www.atomer.sakuran.ne.jp
 // @description 自分のウォッチリストで現在時間から２４時間以内に更新したユーザーを強調表示する
-// @include	 http://www.nicovideo.jp/my/watchlist*
-// @version	 0.6.2
+// @include	 http://www.nicovideo.jp/my/top/user
+// @version	 0.7
 // ==/UserScript==
 (function(window, loaded){
 	var win;
@@ -24,24 +24,34 @@
 	 */
 	function getReportType(s) {
 		s = s.replace(/^\s+|\s+$/g, "");
-		if (/^動画.+投稿しました。$/.test(s)) {
+		// 動画投稿
+		if (/動画を投稿しました。$/.test(s)) {
 			return "video";
-		} else if (/^イラスト.+投稿しました。$/.test(s)) {
+		// 静画投稿
+		} else if (/イラストを投稿しました。$/.test(s)) {
 			return "illust";
-		} else if (/^イラスト.+をクリップしました。$/.test(s)) {
+		// 静画クリップ
+		} else if (/イラストをクリップしました。$/.test(s)) {
 			return "clip";
-		} else if (/開始しました。$/.test(s)) {
+		// 生放送
+		} else if (/生放送を開始しました。$/.test(s)) {
 			return "live";
+		// マイリスト登録
 		} else if (/マイリスト登録しました。$/.test(s)) {
 			return "mylist";
+		// バッジ取得とか
 		} else if (/取得しました。$/.test(s)) {
 			return "stamp";
+		// なんだっけ
 		} else if (/紹介されました。$/.test(s)) {
 			return "intro";
+		// 宣伝
 		} else if (/宣伝しました。$/.test(s)) {
 			return "advert";
+		// 動画再生記録
 		} else if (/達成しました。$/.test(s)) {
 			return "achiev";
+		// その他
 		} else {
 			return "none";
 		}
@@ -49,7 +59,6 @@
 	
 	var HOURS_24 = 1000 * 60 * 60 * 24,
 		styles = {
-			BASE: "myContBody",
 			ITEM_BG_COLOR: {
 				video: "#FDD",
 				illust: "#9E9",
@@ -63,37 +72,29 @@
 				none: "#DDD"
 			},
 			DATE_STYLE: {
-				color: "#F33",
 				fontWeight: "bold"
 			},
-			FILTER_BUTTON: "position:absolute;top:5px;right:5px;width:170px;padding:2px;border:1px solid #F33;",
-			FILTER_ON_BUTTON: "text-align:center;background:#F33;color:#FFF",
-			FILTER_OFF_BUTTON: "text-align:center;background:#FFF;color:#F33",
 			COLORING_DAY_SELECTOR: "position:absolute;top:5px;right:185px;width:100px;"
 		},
 		nodes = {
-			WATCH_LIST_ITEM: ".myContList > LI",
-			DATE: ".report > H4 + P + P",
-			CAPTION: ".report > H4 + P"
+			SWITCH_BASE: "#nicorepo > H3",
+			WATCH_LIST_ITEM: ".timeline .log",
+			DATE: ".relative",
+			CAPTION: ".log-body"
 		},
 		CSS_VISITED = "http://www.atomer.sakura.ne.jp/js/greasemonkey/todaywatch/override_visited.css",
 		CLASS_NEWER_LIST = "ntw_newer",
 		CLASS_VISITED = "ntw_visited",
 		
-		CLASS_FILTER_BUTTON = "ntw_filter",
-		CLASS_FILTER_ON = "ntw_filter_on",
-		CAPTION_FILTER_ON = "最新更新者のみ表示",
-		CAPTION_FILTER_OFF = "すべて表示する",
+		BEFORE_CLASS_DATA_ATTR = "data-nicotodaywatch-beforeclass",
 		
-		SEIGA_URI = "http://lohas.nicoseiga.jp/thumb/${id}i",
-
 		DAY_SET = {
 			"1": "1日以内",
 			"3": "3日以内",
 			"7": "１週間以内",
 			"31": "１ヶ月以内"
 		};
-
+	
 	function getHours(day) {
 		return day * HOURS_24;
 	}
@@ -108,11 +109,11 @@
 
 		_beforeCache: "",
 		
-		_filtering: false,
-		
 		init: function() {
 			this.trigger();
 			this.loadStyle();
+			this.createSwitch();
+			this.em();
 		},
 		
 		loadStyle: function() {
@@ -146,44 +147,14 @@
 			return selector.join("");
 		},
 		
-		createSwitch: function(filtering) {
-			var base = document.getElementById(styles.BASE),
-				target = base.querySelector(".spBox"),
+		createSwitch: function() {
+			var base = document.querySelector(nodes.SWITCH_BASE),
+				target = base.querySelector(".a"),
 				div = document.createElement("div"),
-				defClass = CLASS_FILTER_BUTTON,
-				defStyle = styles.FILTER_BUTTON,
 				selector, defCaption, that = this;
 			
-			if (filtering) {
-				defClass = CLASS_FILTER_BUTTON + " " + CLASS_FILTER_ON;
-				defStyle += styles.FILTER_ON_BUTTON;
-				defCaption = CAPTION_FILTER_OFF;
-			} else {
-				defClass = CLASS_FILTER_BUTTON;
-				defStyle += styles.FILTER_OFF_BUTTON;
-				defCaption = CAPTION_FILTER_ON;
-			}
-			div.setAttribute("style",  defStyle);
-			div.innerHTML = '<a role="button" style="cursor:pointer;display:block;width:100%;height:100%;" class="' + defClass + '">' + defCaption + '</a>';
-			base.insertBefore(div, target);
 			base.parentNode.style.position = "relative";
 			
-			base.querySelector("." + CLASS_FILTER_BUTTON).addEventListener("click", function(e) {
-				if (this.className.indexOf(CLASS_FILTER_ON) === -1) {
-					this.parentNode.setAttribute("style", styles.FILTER_BUTTON + styles.FILTER_ON_BUTTON);
-					this.textContent = CAPTION_FILTER_OFF;
-					this.className = CLASS_FILTER_BUTTON + " " + CLASS_FILTER_ON;
-					that._filtering = true;
-					that.filter(true);
-				} else {
-					this.parentNode.setAttribute("style", styles.FILTER_BUTTON + styles.FILTER_OFF_BUTTON);
-					this.textContent = CAPTION_FILTER_ON;
-					this.className = CLASS_FILTER_BUTTON;
-					that._filtering = false;
-					that.filter(false);
-				}
-			}, false);
-
 			div = document.createElement("div");
 			div.setAttribute("style",  styles.COLORING_DAY_SELECTOR);
 			selector = this._createSelector(DAY_SET, this._day);
@@ -195,55 +166,68 @@
 		},
 
 		changeDay: function(day) {
-			var ul = document.querySelector(".myContList");
+			var ul = document.querySelector(".timeline");
 			
 			this._day = day;
-			this._beforeCache && (ul.innerHTML = this._beforeCache);
-			this.em(this._filtering);
-			this.addIllustImage();
+			this.em();
 		},
 		
 		trigger: function() {
 			var that = this;
 			
-			initializer.setTrigger("html", function() {
-				that.createSwitch(that._filtering);
-				that.em(that._filtering);
-				that.addIllustImage();
-			}, function(s) {
-				return s.indexOf("myContHead") !== -1;
+			initializer.setTrigger("append", function(list) {
+				that.em(list[0]);
+			}, function(list) {
+				return !!list.selector;
 			});
 		},
 		
-		em: function(filtering) {
-			var ul = document.querySelector(".myContList"),
-				watchList = ul.querySelectorAll(nodes.WATCH_LIST_ITEM),
+		em: function(list) {
+			var watchList = list ? list.querySelectorAll(nodes.WATCH_LIST_ITEM) :
+									document.querySelectorAll(nodes.WATCH_LIST_ITEM),
+				beforeClass,
 				day, s, d, cap, t,
 				NOW = +new Date;
-
-			this._beforeCache = ul.innerHTML;
 			
 			for (var i = 0, len = watchList.length; i < len; i++) {
 				day = watchList[i].querySelector(nodes.DATE);
 				if (!day) {
-					filtering && (watchList[i].style.display = "none");
 					continue;
 				}
-				s = day.innerHTML.replace(/^(\d{2})年(\d{2})月(\d{2})日\([^ ]+\) (\d{2}):(\d{2})/, "20$1/$2/$3 $4:$5:00");
+				s = day.getAttribute("datetime");
 				d = new Date(s);
+				
+				// 更新時間比較
 				if (NOW - d.getTime() < getHours(this._day)) {
-					watchList[i].className += " " + CLASS_NEWER_LIST;
-					cap = watchList[i].querySelector(nodes.CAPTION);
-					if (!cap) {
+					// 既に効果適用済み
+					if (watchList[i].className.indexOf(CLASS_NEWER_LIST) !== -1) {
 						continue;
 					}
-					t = getReportType(cap.textContent);
-					watchList[i].style.backgroundColor = styles.ITEM_BG_COLOR[t];
-					day.style.color = styles.DATE_STYLE.color;
-					day.style.fontWeight = styles.DATE_STYLE.fontWeight;
-					this.addVisitedStyle(cap, t);
+					
+					// クラスに効果適用フラグを設定
+					beforeClass = watchList[i].className;
+					watchList[i].setAttribute(BEFORE_CLASS_DATA_ATTR, beforeClass);
+					watchList[i].className = beforeClass + " " + CLASS_NEWER_LIST;
+					
+					// 更新のタイプを見てリストアイテムのバックグラウンドカラー変更
+					cap = watchList[i].querySelector(nodes.CAPTION);
+					if (cap) {
+						t = getReportType(cap.textContent);
+						watchList[i].style.backgroundColor = styles.ITEM_BG_COLOR[t];
+						day.style.fontWeight = styles.DATE_STYLE.fontWeight;
+						this.addVisitedStyle(cap, t);
+					}
 				} else {
-					filtering && (watchList[i].style.display = "none");
+					// スタイルを元に戻す
+					beforeClass = watchList[i].getAttribute(BEFORE_CLASS_DATA_ATTR);
+					beforeClass && (watchList[i].className = beforeClass);
+					watchList[i].removeAttribute(BEFORE_CLASS_DATA_ATTR);
+					
+					cap = watchList[i].querySelector(nodes.CAPTION);
+					if (cap) {
+						watchList[i].style.backgroundColor = "";
+						day.style.fontWeight = "";
+					}
 				}
 			}
 		},
@@ -267,61 +251,22 @@
 			}
 			c = target.className;
 			target.setAttribute("class", c + " " + CLASS_VISITED);
-		},
-		
-		filter: function(filtering) {
-			var watchList, display;
-
-			if (filtering) {
-				display = "none";
-				watchList = document.querySelectorAll(nodes.WATCH_LIST_ITEM + ":not(." + CLASS_NEWER_LIST + ")");
-			} else {
-				display = "block";
-				watchList = document.querySelectorAll(nodes.WATCH_LIST_ITEM);
-			}
-			display = filtering ? "none" : "block";
-			for (var i = 0, len = watchList.length; i < len; i++) {
-				watchList[i].style.display = display;
-			}
-		},
-		
-		addIllustImage: function() {
-			var watchList = document.querySelectorAll(nodes.WATCH_LIST_ITEM),
-			cap, t, report, id, img, a;
-			
-			for (var i = 0, len = watchList.length; i < len; i++) {
-				cap = watchList[i].querySelector(nodes.CAPTION);
-				if (!cap) {
-					continue;
-				}
-				t = getReportType(cap.textContent);
-				if (t === "illust" || t === "clip") {
-					a = cap.querySelector("A");
-					id = a.href.replace(/^.+\/im(\d+)$/, "$1");
-					img = document.createElement("img");
-					img.setAttribute("src", SEIGA_URI.replace("${id}", id));
-					img.setAttribute("height", "48");
-					img.setAttribute("style", "margin-left:6px;");
-					img.setAttribute("class", "seiga_item");
-					a.appendChild(img);
-				}
-			}
 		}
 	};
 	
 	var initializer = (function(win) {
 		var trigger = {
-			html: []
-		},
-			fHTML = win.jQuery.fn.html;
+				append: []
+			},
+			fAppend = win.jQuery.fn.append;
 		
-		win.jQuery.fn.html = function(s) {
+		win.jQuery.fn.append = function(s) {
 			if (!arguments.length) {
-				return fHTML.apply(this, arguments);
+				return fAppend.apply(this, arguments);
 			}
-			fHTML.apply(this, arguments);
-			for (var i = 0, len = trigger.html.length; i < len; i++) {
-				trigger.html[i](s);
+			fAppend.apply(this, arguments);
+			for (var i = 0, len = trigger.append.length; i < len; i++) {
+				trigger.append[i](s);
 			}
 			return this;
 		};
@@ -330,9 +275,9 @@
 				!trigger[type] && (trigger[type] = []);
 				trigger[type].push(function(s) {
 					if (judge) {
-						judge(s) && callback();
+						judge(s) && callback.apply(null, arguments);
 					} else {
-						callback();
+						callback.apply(null, arguments);
 					}
 				});
 			}
