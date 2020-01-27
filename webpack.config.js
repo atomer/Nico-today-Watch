8235bc66e52a65f11d5693a68b1b044d898e1123
atomer <ryota.agata@gmail.com>
2020-01-27 13:37:00 +0900

    Setting ts

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,30 @@
 const path = require('path');
 
 module.exports = {
-  entry: './index.js',
+  mode: 'development',
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader"
+          }
+        ]
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
+    ]
+  },
+  entry: './libs/index.ts',
   output: {
     filename: 'todaywatch.user.js',
     path: path.resolve(__dirname, 'dist'),
